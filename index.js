e4ed470814b8b979623ba058bd5459d9bb193945
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-13 16:43:48 -0700

    add rendering for timer

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //grab elements from DOM
 const header = document.querySelector(".timer__header");
 const titles = document.querySelectorAll("h1");
-const day = document.querySelector(".timer__day");
-const hour = document.querySelector(".timer__hour");
-const minute = document.querySelector(".timer__minute");
-const second = document.querySelector(".timer__second");
+const dayItem = document.querySelector(".timer__day");
+const hourItem = document.querySelector(".timer__hour");
+const minuteItem = document.querySelector(".timer__minute");
+const secondItem = document.querySelector(".timer__second");
+const dayContainer = document.querySelector(".timer__item--day");
+const hourContainer = document.querySelector(".timer__item--hour");
+const minContainer = document.querySelector(".timer__item--min");
+const secContainer = document.querySelector(".timer__item--sec");
 
 //test area
 
@@ -38,8 +42,15 @@ function grandOpeningCounter(start, end) {
     hours = hours - (days * 24);
     minutes = minutes - (days * 24 * 60) - (hours * 60);
     seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
-   // day.textContent = 
-    return `${days} : ${hours} : ${minutes} : ${seconds}`;
+    dayItem.textContent = `${days} :`;
+    dayContainer.append(dayItem);
+    hourItem.textContent = `${hours} :`;
+    hourContainer.append(hourItem);
+    minuteItem.textContent = `${minutes} :`;
+    minContainer.append(minuteItem);
+    secondItem.textContent = `${seconds}`;
+    secContainer.append(secondItem);
+    //return `${days} : ${hours} : ${minutes} : ${seconds}`;
 }
 
 const startDate = new Date();
